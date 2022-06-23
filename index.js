c23abd1a55f7241dd8d75569bb64cf0c0a6cd615
ChrisNieves <nievescs20@gmail.com>
2022-06-23 17:14:25 -0400

    added checkpoints array

--- index.js
+++ index.js
@@ -2,21 +2,31 @@ const shell = require("shelljs");
 
 //add your students github handles
 const students = [
-  "vicantlop",
-  "tpham187",
-  "KristinLevine",
-  "emrebsbydr",
-  "Burkelbear",
-  "hitgub8",
-  "chrisru-dotcom",
-  "leebrian33",
-  "wattychen",
-  "rgomez07",
-  "mcombs289",
-  "kevinliang327",
+  "student-github",
+  "student-github",
+  "student-github",
+  "student-github",
+  "student-github",
+  "student-github",
+  "student-github",
+  "student-github",
+  "student-github",
+  "student-github",
+  "student-github",
+  "student-github",
 ];
 //change the name of checkpoint you're going to check
-const checkpoint = "Checkpoint.DOM";
+//these are the previous names of the checkpoints. possible that they may have changed
+
+const checkpoints = [
+  "Checkpoint.DOM",
+  "Checkpoint-Node-Express",
+  "Checkpoint-Pillars-v2",
+  "Checkpoint-React-v2",
+  "Checkpoint-Redux",
+];
+
+const checkpoint = checkpoints[0];
 
 shell.exec(`mkdir ${checkpoint}`);
 
@@ -25,5 +35,3 @@ for (let student of students) {
     `git clone git@github.com:${student}/${checkpoint}.git ../${checkpoint}/${student}`
   );
 }
-
-//just run this file in node:)
